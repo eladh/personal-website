a3be3a05c2c06e294b5f3a60892aec024cd2ee5e
elad-ts <146168076+elad-ts@users.noreply.github.com>
2024-07-17 23:47:54 +0300

    add public to github action build

--- src/components/Layout/Navbar/NavLinks.js
+++ src/components/Layout/Navbar/NavLinks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'gatsby-link';
 import { Link as SLink } from 'react-scroll';
 
 const NavItemsSmoothLinks = ({ NavItem }) => (
@@ -20,15 +21,43 @@ const NavItemsSmoothLinks = ({ NavItem }) => (
     </NavItem>
     <NavItem>
       <SLink smooth offset={-100} hashSpy to="contact">
-        Reachout
+        Contact
       </SLink>
     </NavItem>
   </>
 );
 
+const NavItemsGatsbyLinks = ({ NavItem }) => (
+  <>
+    <NavItem>
+      <Link to="/">Home</Link>
+    </NavItem>
+    <NavItem>
+      <Link to="/#about">About</Link>
+    </NavItem>
+    <NavItem>
+      <Link to="/#evangelism">Evangelism</Link>
+    </NavItem>
+    <NavItem>
+      <Link to="/#contact">Contact</Link>
+    </NavItem>
+  </>
+);
+
 const NavLinks = React.memo(({ NavItem }) => {
+  let path = null;
+  if (typeof window !== 'undefined') {
+    path = window.location.pathname;
+  }
+
   return (
-      <NavItemsSmoothLinks NavItem={NavItem} />
+    <>
+      {path === '/' ? (
+        <NavItemsSmoothLinks NavItem={NavItem} />
+      ) : (
+        <NavItemsGatsbyLinks NavItem={NavItem} />
+      )}
+    </>
   );
 });
 
